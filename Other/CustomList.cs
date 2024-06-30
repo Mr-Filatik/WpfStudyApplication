6d53f9c87b5f5140ebae5ee858ac01fa995ecbbe
Vladislav Filatov <v.filatov@softchamber.ru>
2024-06-30 12:43:06 +0400

    Add CustomList

---
+++ Other/CustomList.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+
+namespace Other
+{
+    public class CustomList<T> : IEnumerable<T>, IEnumerable, IEnumerator<T>, IEnumerator, IDisposable
+    {
+        #region Private variables
+
+        private T[] _array;
+        private int _lenght;
+
+        #endregion
+
+        #region Properties and Indexer
+
+        public int Capacity => _array.Length;
+
+        public int Lenght => _lenght;
+
+        public T this[int index]
+        {
+            get
+            {
+                return _array[index];
+            }
+            set
+            {
+                _array[index] = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public CustomList() : this(4) { }
+
+        public CustomList(int capacity)
+        {
+            _array = new T[capacity];
+            _lenght = 0;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Add(T item)
+        {
+            if (_lenght == Capacity)
+            {
+                LenghtIncrease();
+            }
+            _array[_lenght] = item;
+            _lenght++;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            var count = collection.Count();
+            if (_lenght + count > Capacity)
+            {
+                LenghtIncrease(count);
+            }
+            foreach (var item in collection)
+            {
+                _array[_lenght] = item;
+                _lenght++;
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (_lenght == Capacity) //Lenght + 1 > Capacity
+            {
+                LenghtIncrease();
+            }
+            var currentArray = _array;
+            _array = new T[Capacity];
+            var addIndex = 0;
+            for (int i = 0; i < _lenght; i++)
+            {
+                if (i == index)
+                {
+                    _array[i + addIndex] = item;
+                    addIndex = 1;
+                }
+                _array[i + addIndex] = currentArray[i];
+            }
+            _lenght++;
+        }
+
+        public bool Remove(T item)
+        {
+            var addIndex = 0;
+            var output = false;
+            for (int i = 0; i < _lenght; i++)
+            {
+                _array[i - addIndex] = _array[i];
+                if (_array[i - addIndex].Equals(item)) // == don't work with generics
+                {
+                    addIndex = 1;
+                    output = true;
+                }
+            }
+            _lenght--;
+            return output;
+        }
+
+        public bool RemoveAt(int index)
+        {
+            var addIndex = 0;
+            var output = false;
+            for (int i = 0; i < _lenght; i++)
+            {
+                _array[i - addIndex] = _array[i];
+                if (i == index)
+                {
+                    addIndex = 1;
+                    output = true;
+                }
+            }
+            _lenght--;
+            return output;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void LenghtIncrease()
+        {
+            var currentArray = _array;
+            int newCapacity = Capacity * 2;
+            _array = new T[newCapacity];
+            for (int i = 0; i < currentArray.Length; i++)
+            {
+                _array[i] = currentArray[i];
+            }
+        }
+
+        private void LenghtIncrease(int count)
+        {
+            var currentArray = _array;
+            int newCapacity = Capacity * 2;
+            while (_lenght + count > newCapacity)
+            {
+                newCapacity *= 2;
+            }
+            _array = new T[newCapacity];
+            for (int i = 0; i < currentArray.Length; i++)
+            {
+                _array[i] = currentArray[i];
+            }
+        }
+
+        #endregion
+
+        #region IEnumerator and IEnumerable
+
+        private int _indexEnumerable = -1;
+
+        public T Current => _array[_indexEnumerable];
+
+        object IEnumerator.Current => _array[_indexEnumerable];
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (_indexEnumerable + 1 < _lenght)
+            {
+                _indexEnumerable++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _indexEnumerable = -1;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+            //+ some code
+        }
+
+        #endregion
+    }
+
+    public static class CustomListExtention
+    {
+        public static T[] ToArray<T>(this CustomList<T> customList)
+        {
+            var array = new T[customList.Lenght];
+            for (int i = 0; i < customList.Lenght; i++)
+            {
+                array[i] = customList[i];
+            }
+            return array;
+        }
+
+        public static List<T> ToList<T>(this CustomList<T> customList)
+        {
+            var list = new List<T>();
+            for (int i = 0; i < customList.Lenght; i++)
+            {
+                list.Add(customList[i]);
+            }
+            return list;
+        }
+    }
+}
--- WpfStudyApplication/MainWindow.xaml.cs
+++ WpfStudyApplication/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Other;
+using System.Windows;
 using WpfStudyApplication.Database.Abstract;
 using WpfStudyApplication.Database.Exceptions;
 
@@ -22,19 +23,63 @@ namespace WpfStudyApplication
 
         private void LoginInApplication(object sender, RoutedEventArgs e)
         {
-            try
+            CustomList<int> list = new CustomList<int>();
+
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            list.Add(1);
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Show(list);
+
+            list.AddRange(new int[] { 1, 2 });
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Show(list);
+
+            list.Insert(1, 22);
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            list.AddRange(new int[] { 1, 2, 3, 4 });
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            list.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 });
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Show(list);
+
+            list.Remove(22);
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Show(list);
+
+            list.RemoveAt(2);
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Show(list);
+
+            list.Add(19);
+            MessageBox.Show($"Lenght: {list.Lenght}, Capacity: {list.Capacity}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Show(list);
+
+            //try
+            //{
+            //    var librarian = _librarianRepository.LoginLibrarianByEmailAndPassword(emailBox.Text, passwordBox.Text);
+            //    //save
+            //    MessageBox.Show("OK", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
+            //catch (EntityNotFoundException ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //}
+            //catch (PasswordNotCorrectException ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //}
+
+            void Show<T>(IEnumerable<T> values)
             {
-                var librarian = _librarianRepository.LoginLibrarianByEmailAndPassword(emailBox.Text, passwordBox.Text);
-                //save
-                MessageBox.Show("OK", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            catch (EntityNotFoundException ex)
-            {
-                MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            catch (PasswordNotCorrectException ex)
-            {
-                MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                string str = "";
+                foreach (T value in values)
+                {
+                    str += $"{value}, ";
+                }
+                MessageBox.Show($"{str}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
     }
